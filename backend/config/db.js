d16b2a8d6d278861542dd2463e4b85d9eb7bc93c
anikas <22bec020@nith.ac.in>
2025-11-15 13:17:40 +0530

    db fixed

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,15 @@
 import mongoose from "mongoose";
+
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URL);
+    await mongoose.connect(process.env.MONGODB_URL, {
+      tls: true,
+      tlsInsecure: true,
+    });
     console.log("db connected");
   } catch (error) {
-    console.log(error);
+    console.log("DB Error:", error.message);
   }
 };
+
 export default connectDB;
--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+
 dotenv.config();
-const port = process.env.PORT || 5000;
+
 const app = express();
+const port = process.env.PORT || 5000;
+
+connectDB();
+
 app.get("/", (req, res) => {
   res.send("hello");
 });
+
 app.listen(port, () => {
-  connectDB();
-  console.log("started");
+  console.log("Server started on port", port);
 });
